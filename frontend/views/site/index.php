e692c5d9715ecb18c8a96a698c745aa27b980e0b
Faisal Rahman <fr.faisalrahman21@gmail.com>
2015-04-11 21:42:44 +0700

    magic happens

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -1,4 +1,6 @@
 <?php
+
+use rmrevin\yii\fontawesome\FA;
 $this->title = 'Pusat Perencanaan Karir';
 ?>
 <!-- Masthead Section -->
@@ -18,7 +20,7 @@ $this->title = 'Pusat Perencanaan Karir';
 <div class="container">
 	<div class="row text-center">
 		<div class="col-md-4" id="kuesioner-kesiapan">
-			<button class="btn btn-warning btn-circle btn-outline"><?php echo FA::icon('cog')->spin(); ?></button>
+			<button class="btn btn-primary btn-circle btn-outline"><?php echo FA::icon('briefcase')->size(FA::SIZE_2X); ?></button>
 		</div>
 		<div class="col-md-4" id="kuesioner-karir">Karir yang Tepat untuk Anda</div>
 		<div class="col-md-4" id="kuesioner-kepribadian">Tes Kepribadian Anda</div>
