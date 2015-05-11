a9c3f2f2cc4029f4d71de353b9270878e3df056a
Faisal Rahman <fr.faisalrahman21@gmail.com>
2015-05-11 13:09:02 +0700

    added new composer packages for CSV export capabilities

--- backend/views/site/index.php
+++ backend/views/site/index.php
@@ -1,8 +1,21 @@
 <?php
 /* @var $this yii\web\View */
+use kartik\export\ExportMenu;
+use yii\data\SqlDataProvider;
+use kartik\grid\GridView;
 
 $this->title = 'Dashboard';
 $this->params['breadcrumbs'][] = '';
+
+$dataProvider = new SqlDataProvider([
+		'sql' => 'SELECT * FROM "ANSWER"',
+	]);
 ?>
 <div id="title" class="MuseoSans">Dashboard</div>
+<?php
+echo ExportMenu::widget([
+		'dataProvider' => $dataProvider,
+		'fontAwesome' => true,
+	]);
+ ?>
 
