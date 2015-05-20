90e4571ff8902c5c0dc4c148d0f511c6181e09f3
unid3ntified <unid3ntifiedz@gmail.com>
2015-05-20 16:28:18 +0700

    signup

--- frontend/models/SignupForm.php
+++ frontend/models/SignupForm.php
@@ -13,6 +13,14 @@ class SignupForm extends Model
     public $username;
     public $email;
     public $password;
+	public $date_of_birth;
+	public $place_of_birth;
+	public $address;
+	public $gender;
+	public $phone;
+	public $education;
+	public $occupation;
+	
 
     /**
      * @inheritdoc
@@ -32,6 +40,22 @@ class SignupForm extends Model
 
             ['password', 'required'],
             ['password', 'string', 'min' => 6],
+			
+			['place_of_birth', 'filter', 'filter' => 'trim'],
+            ['place_of_birth', 'string', 'max' => 30],
+			
+			['date_of_birth', 'filter', 'filter' => 'trim'],
+            ['date_of_birth', 'string', 'max' => 10],
+			
+			['address', 'filter', 'filter' => 'trim'],
+            ['address', 'string', 'max' => 50],
+			
+			['phone', 'filter', 'filter' => 'trim'],
+            ['phone', 'string', 'max' => 13],
+			
+			['occupation', 'filter', 'filter' => 'trim'],
+            ['occupation', 'string', 'max' => 30],
+            
         ];
     }
 
@@ -47,6 +71,15 @@ class SignupForm extends Model
             $user->username = $this->username;
             $user->email = $this->email;
             $user->setPassword($this->password);
+			$user->role = 'u';
+			$user->date_of_birth = $this->date_of_birth;
+			$user->place_of_birth = $this->place_of_birth;
+			$user->address = $this->address;
+			$user->gender = $this->gender;
+			$user->phone = $this->phone;
+			$user->education = $this->education;
+			$user->occupation = $this->occupation;
+			
             $user->generateAuthKey();
             if ($user->save()) {
                 return $user;
--- frontend/views/site/signup.php
+++ frontend/views/site/signup.php
@@ -20,6 +20,13 @@ $this->params['breadcrumbs'][] = $this->title;
                 <?= $form->field($model, 'username') ?>
                 <?= $form->field($model, 'email') ?>
                 <?= $form->field($model, 'password')->passwordInput() ?>
+				<?= $form->field($model, 'place_of_birth') ?>
+				<?= $form->field($model, 'date_of_birth') ?>
+				<?= $form->field($model, 'address') ?>
+				<?= $form->field($model, 'gender')->dropDownList(['Pria','Wanita'], ['prompt'=>'Jenis kelamin']) ?>
+				<?= $form->field($model, 'phone') ?>
+				<?= $form->field($model, 'education')->dropDownList(['SD','SMP','SMA','D3','S1','S2','S3'], ['prompt'=>'Pendidikan terakhir']) ?>
+				<?= $form->field($model, 'occupation') ?>
                 <div class="form-group">
                     <?= Html::submitButton('Signup', ['class' => 'btn btn-primary', 'name' => 'signup-button']) ?>
                 </div>
