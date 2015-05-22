7b347bc61a944cfaec52656a97ee671bdc5a9de3
Faisal Rahman <fr.faisalrahman21@gmail.com>
2015-05-23 03:32:35 +0700

    polishing interface

--- frontend/views/user/update.php
+++ frontend/views/user/update.php
@@ -5,17 +5,21 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model common\models\User */
 
-$this->title = 'Update User: ' . ' ' . $model->username;
+$this->title = 'Perbarui Profil: ' . ' ' . $model->username;
 $this->params['breadcrumbs'][] = ['label' => 'Users', 'url' => ['index']];
 $this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id]];
 $this->params['breadcrumbs'][] = 'Update';
 ?>
 <div class="container">
+	<div class="inset-wrapper inset-padded">
+		<h1 class="MuseoSlab-500 header-text"><?= Html::encode($this->title) ?></h1>
+		<hr>
 
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <?= $this->render('_form', [
+		<?= $this->render('_form', [
         'model' => $model,
     ]) ?>
+	</div>
+
+    
 
 </div>
